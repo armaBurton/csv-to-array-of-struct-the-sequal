042ae2e995290e95b0762230602e228b532dbbe9
Arma Burton <arma@armaburton.com>
2023-05-01 19:30:21 -0700

    testing read data

--- main.cpp
+++ main.cpp
@@ -4,5 +4,21 @@
 int main(void){
   FILE *file;
 
-  file = fopen("")
+  file = fopen("./data/dirtbikeSpecs/dirtbikeData.txt", "r");
+
+  if (file == NULL){
+    printf("Error opening file.\n");
+  }
+
+  DirtbikeSpecs dirtbikeSpecs[100];
+
+  int read = 0,
+      records = 0;
+    
+  do{
+    
+
+  }while(!feof(file));
+
+
 }
